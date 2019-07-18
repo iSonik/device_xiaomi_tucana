2f05b0f22ce6cbaba7e00111383a2d0e9f12321e
LuK1337 <priv.luk@gmail.com>
2019-07-18 19:41:39 +0200

    tucana: Set proper fingerprint sensor location
    Change-Id: I3cfc4d198c3a88724e3a268a4e9e9b7ab9909f9b

--- device.mk
+++ device.mk
@@ -11,7 +11,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/tucana/tucana-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Fingerprint
 PRODUCT_PACKAGES += \
