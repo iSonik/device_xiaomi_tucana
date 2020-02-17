ae5f4b5a9a06fb9c48c9d0c7f0f8432e531a745b
Pig <pig.priv@gmail.com>
2020-02-17 15:43:03 +0100

    tucana: fod: Remove duplicated setTouchMode onShowFODView
    Change-Id: I8936f97201bff64b7d526b4f974bf929d5e7c3a6
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -100,7 +100,6 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
-    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
     return Void();
 }
 
