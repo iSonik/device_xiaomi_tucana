9fb87fe1f608c7135590667955dfd894c3f8da4b
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-03-29 19:44:59 -0300

    tucana: Initial properties
    * Also update Screen Density value
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,12 +5,16 @@
 #
 
 # Display density
-TARGET_SCREEN_DENSITY := 480
+TARGET_SCREEN_DENSITY := 440
 
 # Inherit from sm6150-common
 -include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/tucana
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := tucana
